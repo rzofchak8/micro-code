536642d12e3bf2b4ab39e9b6ae5ffbcda5b5e80a
rzofchak8 <r.zofchak8@gmail.com>
2023-08-31 22:57:33 -0400

    added constants to config

--- arduino/ZofCloudConfig.h
+++ arduino/ZofCloudConfig.h
@@ -5,10 +5,19 @@
 #define WIFI_PASSWORD "wifi password"
 #define SERVER_IP "server ip"
 #define SERVER_PORT 0000
+#define CLIENT_TIMEOUT 1000
 
 #define MOISTURE_MAX 400
 #define MOISTURE_AIR 800
 #define LIGHT_MAX 1100
 #define LIGHT_MIN 0
+
+#define LFS_MBED_RP2040_VERSION_MIN_TARGET      "LittleFS_Mbed_RP2040 v1.1.0"
+#define LFS_MBED_RP2040_VERSION_MIN             1001000
+
+#define _LFS_LOGLEVEL_          1
+#define RP2040_FS_SIZE_KB       64
+
+#define FORCE_REFORMAT          false
 
 #endif // ZOFCLOUDCONFIG_H
